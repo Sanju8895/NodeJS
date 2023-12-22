7c2e4686f4f4a0a9e2618501e6c990ca32bd68a2
Sanjaya Nayak <sanjayanayak67@gmail.com>
2023-12-23 01:04:20 +0530

    1st assignment

---
+++ Assignments/assignment_1.js
@@ -0,0 +1,25 @@
+let args = process.argv.slice(2);
+const [num1, num2, operator] = args;
+function evaluation(num1, num2, operator) {
+  switch (operator) {
+    case '+':
+      console.log(num1 + num2);
+      break;
+    case '-':
+      console.log(num1 - num2);
+      break;
+    case '*':
+      console.log(num1 * num2);
+      break;
+    case '/':
+      if (num2 === 0) {
+        console.log('Error: Cannot divide by zero');
+      } else {
+        console.log(num1 / num2);
+      }
+      break;
+    default:
+      console.log('Error: Invalid operator');
+  }
+}
+evaluation(+num1,+num2, operator);
